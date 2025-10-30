5e7e4ea54234995a57c95dfaa4812b19bdab2ac9
Nguyen Le Quoc Dat <quocdat2601@gmail.com>
2025-10-30 21:50:15 +0700

    Handle Refund 1.1

---
+++ Application/DTOs/Refund/RefundResultDto.cs
@@ -0,0 +1,13 @@
+﻿using PublicCarRental.Infrastructure.Data.Models;
+
+namespace PublicCarRental.Application.DTOs.Refund
+{
+    public class RefundResultDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public int RefundId { get; set; }
+        public RefundStatus Status { get; set; }
+        public string? PayoutTransactionId { get; set; }
+    }
+}
---
+++ Application/Service/Pay/IPayOSPayoutService.cs
@@ -0,0 +1,13 @@
+﻿using PublicCarRental.Application.DTOs.Pay;
+
+namespace PublicCarRental.Application.Service.Pay
+{
+    public interface IPayOSPayoutService
+    {
+        Task<PayoutResult> CreateSinglePayoutAsync(int refundId, BankAccountInfo bankInfo);
+        Task<PayoutInfo> GetPayoutStatusAsync(string payoutId);
+        Task<decimal> GetAccountBalanceAsync();
+        Task<decimal> EstimateCreditAsync(decimal amount);
+
+    }
+}
